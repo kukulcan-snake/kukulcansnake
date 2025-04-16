763c7118f79378616c8e252e46bdf1dda71bed24
kukulcan-snake <kukulcansnake@gmail.com>
2025-04-16 21:29:53 +0800

    Add a new source option

--- src/ClientOrder.js
+++ src/ClientOrder.js
@@ -448,6 +448,7 @@ function OrderForm({productInfo, fetch}) {
                                         <option value="小靜">小靜しずか</option>
                                         <option value="茶茶狐">茶茶狐</option>
                                         <option value="巫巫巫巫巫巫巫">巫巫巫巫巫巫巫</option>
+                                        <option value="諾拉公主">諾拉公主</option>
                                         <option value="被蛇蛇拐來的">被蛇蛇拐來的</option>
                                         <option value="其他">其他</option>
                                     </select>
