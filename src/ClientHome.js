be1db7f5b3d5ece0b422a28ca66557996597479f
kukulcan-snake <kukulcansnake@gmail.com>
2026-07-13 19:58:04 +0800

    Update ClientHome.js

--- src/ClientHome.js
+++ src/ClientHome.js
@@ -40,7 +40,7 @@ const ClientHome = () => {
                 <img src={require("./image/menu2.jpg")} alt="menuImg2" className="menuImg"/>
             </div>
             <div className="banner menuBox whiteBg">
-                <img src={require("./image/certificate.png")} alt="certificate" className="certImg"/>
+                <img src=<img src="https://lh3.googleusercontent.com/d/1qXFbF93QyHvitvTY0wSBuqm5gF80EavF" alt="certificate" className="certImg"/>
             </div>
             <div className="banner grayBg">
                 <p>下完訂單記得告訴蛇蛇歐:D</p>
