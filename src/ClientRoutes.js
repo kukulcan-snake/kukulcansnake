1794f5f0890cb2d1819837c72122df0168512a78
kukulcan-snake <kukulcansnake@gmail.com>
2026-07-14 14:58:44 +0800

    Update ClientRoutes.js

--- src/ClientRoutes.js
+++ src/ClientRoutes.js
@@ -1,34 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import ClientOrder from './ClientOrder'; // 確保路徑正確
 
 function ClientRoutes() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    // 這裡請務必填入你 Apps Script 最新產生的網址
     const gasUrl = 'https://script.google.com/macros/s/AKfycbxoqaMojjuhvz73CsczAl5820sMPilGMoHboQxKwOC23actQng0-gt-nKS4CNdja2wk/exec';
     
     fetch(gasUrl)
       .then(res => res.json())
       .then(data => {
-        console.log("成功抓取資料:", data);
         setProducts(data);
       })
       .catch(err => console.error("抓取錯誤:", err));
   }, []);
 
-  return (
-    <div>
-      {products.length > 0 ? (
-        <ul>
-          {products.map((p, index) => (
-            <li key={index}>{p.name} - ${p.price}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>載入中...</p>
-      )}
-    </div>
-  );
+  // 這裡只傳遞數據給訂單頁，不直接顯示列表
+  return <ClientOrder productInfo={products} />;
 }
 
 export default ClientRoutes;
