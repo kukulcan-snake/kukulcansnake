02744cbf8ca82cc65624407b31a2469d9559ce27
kukulcan-snake <kukulcansnake@gmail.com>
2026-07-14 15:03:41 +0800

    Update ClientRoutes.js

--- src/ClientRoutes.js
+++ src/ClientRoutes.js
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import ClientOrder from './ClientOrder'; // 確保路徑正確
+import ClientOrder from './ClientOrder';
 
 function ClientRoutes() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const gasUrl = 'https://script.google.com/macros/s/AKfycbxoqaMojjuhvz73CsczAl5820sMPilGMoHboQxKwOC23actQng0-gt-nKS4CNdja2wk/exec';
-    
-    fetch(gasUrl)
+    fetch('https://script.google.com/macros/s/AKfycbxoqaMojjuhvz73CsczAl5820sMPilGMoHboQxKwOC23actQng0-gt-nKS4CNdja2wk/exec')
       .then(res => res.json())
-      .then(data => {
-        setProducts(data);
-      })
-      .catch(err => console.error("抓取錯誤:", err));
+      .then(data => setProducts(data))
+      .catch(err => console.error("Error:", err));
   }, []);
 
-  // 這裡只傳遞數據給訂單頁，不直接顯示列表
   return <ClientOrder productInfo={products} />;
 }
 
