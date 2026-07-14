e24de1e597e486f71bdf5f13a42c96e1be1ef884
kukulcan-snake <kukulcansnake@gmail.com>
2026-07-14 14:56:35 +0800

    Update ClientRoutes.js

--- src/ClientRoutes.js
+++ src/ClientRoutes.js
@@ -4,23 +4,26 @@ function ClientRoutes() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    // 改用這裡的網址，這是我幫你整理的參數設定
-    fetch('https://script.google.com/macros/s/AKfycbxoqgMojjuhvz73CsczA15820sMPi1GMoHboQxKwOC23actQng0-gt-nkS4CNdja2wk/exec', {
-      method: 'GET',
-      mode: 'cors'
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log("資料已抓取:", data);
-      setProducts(data);
-    })
-    .catch(err => console.error("抓取失敗:", err));
+    // 這裡請務必填入你 Apps Script 最新產生的網址
+    const gasUrl = 'https://script.google.com/macros/s/AKfycbxoqaMojjuhvz73CsczAl5820sMPilGMoHboQxKwOC23actQng0-gt-nKS4CNdja2wk/exec';
+    
+    fetch(gasUrl)
+      .then(res => res.json())
+      .then(data => {
+        console.log("成功抓取資料:", data);
+        setProducts(data);
+      })
+      .catch(err => console.error("抓取錯誤:", err));
   }, []);
 
   return (
     <div>
       {products.length > 0 ? (
-        <ul>{products.map(p => <li key={p.product_id}>{p.name}</li>)}</ul>
+        <ul>
+          {products.map((p, index) => (
+            <li key={index}>{p.name} - ${p.price}</li>
+          ))}
+        </ul>
       ) : (
         <p>載入中...</p>
       )}
